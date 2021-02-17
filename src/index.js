350ce00002c71b43163c4f538d169a175f3876b3
Pavel Latyshov <platyshov@MacBook-Pro-Pavel.local>
2021-02-17 20:32:53 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let arr = str.split('')
+  let length = arr.length
+  let i = 0
+  while (i < length) {
+    i++
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] == "(" && arr[i + 1] == ")"
+        || arr[i] == "{" && arr[i + 1] == "}"
+        || arr[i] == "[" && arr[i + 1] == "]"
+        || arr[i] == "|" && arr[i + 1] == "|"
+        || arr[i] == "1" && arr[i + 1] == "2"
+        || arr[i] == "3" && arr[i + 1] == "4"
+        || arr[i] == "5" && arr[i + 1] == "6"
+        || arr[i] == "7" && arr[i + 1] == "7"
+        || arr[i] == "8" && arr[i + 1] == "8") {
+        arr.splice(i, 2)
+      }
+    }
+  }
+  if (arr.length === 0) {
+    return true
+  } else {
+    return false
+  }
 }
